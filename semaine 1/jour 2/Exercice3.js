e1552256a6b98a09d1f06c4dd4b276312fa1c508
RihabMhd <rihabmahdi19@gmail.com>
2026-03-21 18:42:53 +0000

    day 2 done

--- semaine 1/jour 2/Exercice3.js
+++ semaine 1/jour 2/Exercice3.js
@@ -31,34 +31,127 @@
  */
 
 const matchs = [
-  { journee: 1, domicile: 'FUS Rabat',     bDomicile: 2, bExterieur: 1, exterieur: 'WAC'         },
-  { journee: 1, domicile: 'Raja',          bDomicile: 1, bExterieur: 1, exterieur: 'MAS'         },
-  { journee: 1, domicile: 'FAR',           bDomicile: 3, bExterieur: 0, exterieur: 'HUSA'        },
-  { journee: 2, domicile: 'WAC',           bDomicile: 2, bExterieur: 2, exterieur: 'Raja'        },
-  { journee: 2, domicile: 'MAS',           bDomicile: 1, bExterieur: 0, exterieur: 'FAR'         },
-  { journee: 2, domicile: 'HUSA',          bDomicile: 1, bExterieur: 3, exterieur: 'FUS Rabat'   },
-  { journee: 3, domicile: 'Raja',          bDomicile: 2, bExterieur: 0, exterieur: 'FAR'         },
-  { journee: 3, domicile: 'FUS Rabat',     bDomicile: 1, bExterieur: 1, exterieur: 'MAS'         },
-  { journee: 3, domicile: 'WAC',           bDomicile: 4, bExterieur: 1, exterieur: 'HUSA'        },
-  { journee: 4, domicile: 'FAR',           bDomicile: 2, bExterieur: 2, exterieur: 'WAC'         },
-  { journee: 4, domicile: 'MAS',           bDomicile: 0, bExterieur: 1, exterieur: 'FUS Rabat'   },
-  { journee: 4, domicile: 'HUSA',          bDomicile: 2, bExterieur: 3, exterieur: 'Raja'        },
+  { journee: 1, domicile: 'FUS Rabat', bDomicile: 2, bExterieur: 1, exterieur: 'WAC' },
+  { journee: 1, domicile: 'Raja', bDomicile: 1, bExterieur: 1, exterieur: 'MAS' },
+  { journee: 1, domicile: 'FAR', bDomicile: 3, bExterieur: 0, exterieur: 'HUSA' },
+  { journee: 2, domicile: 'WAC', bDomicile: 2, bExterieur: 2, exterieur: 'Raja' },
+  { journee: 2, domicile: 'MAS', bDomicile: 1, bExterieur: 0, exterieur: 'FAR' },
+  { journee: 2, domicile: 'HUSA', bDomicile: 1, bExterieur: 3, exterieur: 'FUS Rabat' },
+  { journee: 3, domicile: 'Raja', bDomicile: 2, bExterieur: 0, exterieur: 'FAR' },
+  { journee: 3, domicile: 'FUS Rabat', bDomicile: 1, bExterieur: 1, exterieur: 'MAS' },
+  { journee: 3, domicile: 'WAC', bDomicile: 4, bExterieur: 1, exterieur: 'HUSA' },
+  { journee: 4, domicile: 'FAR', bDomicile: 2, bExterieur: 2, exterieur: 'WAC' },
+  { journee: 4, domicile: 'MAS', bDomicile: 0, bExterieur: 1, exterieur: 'FUS Rabat' },
+  { journee: 4, domicile: 'HUSA', bDomicile: 2, bExterieur: 3, exterieur: 'Raja' },
 ];
 
 function calculerClassement(matchs) {
-  // TODO
+  const equipes = {};
+
+  function initEquipe(nom) {
+    if (!equipes[nom]) {
+      equipes[nom] = {
+        equipe: nom,
+        joues: 0,
+        victoires: 0,
+        nuls: 0,
+        defaites: 0,
+        bpour: 0,
+        bcontre: 0,
+        diff: 0,
+        points: 0
+      };
+    }
+  }
+
+  matchs.forEach(m => {
+    const { domicile, exterieur, bDomicile, bExterieur } = m;
+
+    initEquipe(domicile);
+    initEquipe(exterieur);
+
+    const home = equipes[domicile];
+    const away = equipes[exterieur];
+
+    home.joues++;
+    away.joues++;
+
+
+    home.bpour += bDomicile;
+    home.bcontre += bExterieur;
+
+    away.bpour += bExterieur;
+    away.bcontre += bDomicile;
+
+    if (bDomicile > bExterieur) {
+      home.victoires++;
+      away.defaites++;
+      home.points += 3;
+    } else if (bDomicile < bExterieur) {
+      away.victoires++;
+      home.defaites++;
+      away.points += 3;
+    } else {
+      home.nuls++;
+      away.nuls++;
+      home.points += 1;
+      away.points += 1;
+    }
+  });
+
+  Object.values(equipes).forEach(e => {
+    e.diff = e.bpour - e.bcontre;
+  });
+
+
+  const classement = Object.values(equipes).sort((a, b) => {
+    return (
+      b.points - a.points ||
+      b.diff - a.diff ||
+      b.bpour - a.bpour ||
+      a.equipe.localeCompare(b.equipe)
+    );
+  });
+
+  classement.forEach((e, i) => {
+    e.rang = i + 1;
+  });
+
+  return classement;
 }
 
 function meilleureAttaque(classement) {
-  // TODO
+  return classement.reduce((max, e) =>
+    e.bpour > max.bpour ? e : max
+  );
 }
 
 function meilleureDefense(classement) {
-  // TODO
+  return classement.reduce((best, e) =>
+    e.bcontre < best.bcontre ? e : best
+  );
 }
 
 function serieInvaincue(matchs, equipe) {
-  // TODO
+  const sorted = [...matchs].sort((a, b) => b.journee - a.journee);
+
+  let count = 0;
+
+  for (const m of sorted) {
+    if (m.domicile !== equipe && m.exterieur !== equipe) continue;
+
+    const estDomicile = m.domicile === equipe;
+    const butsPour = estDomicile ? m.bDomicile : m.bExterieur;
+    const butsContre = estDomicile ? m.bExterieur : m.bDomicile;
+
+    if (butsPour >= butsContre) {
+      count++;
+    } else {
+      break;
+    }
+  }
+
+  return count;
 }
 
 const classement = calculerClassement(matchs);
--- semaine 1/jour 3/Exercice1.js
+++ semaine 1/jour 3/Exercice1.js
@@ -32,35 +32,48 @@
 const entrepot = {
   'Zone-A': {
     'Rayon-A1': [
-      { id: 'P001', nom: 'Clavier',  stock: 45, prixUnitaire: 120 },
-      { id: 'P002', nom: 'Souris',   stock: 3,  prixUnitaire: 85  },
-      { id: 'P003', nom: 'Webcam',   stock: 12, prixUnitaire: 220 },
+      { id: 'P001', nom: 'Clavier', stock: 45, prixUnitaire: 120 },
+      { id: 'P002', nom: 'Souris', stock: 3, prixUnitaire: 85 },
+      { id: 'P003', nom: 'Webcam', stock: 12, prixUnitaire: 220 },
     ],
     'Rayon-A2': [
-      { id: 'P004', nom: 'Ecran 24"',stock: 8,  prixUnitaire: 1500 },
-      { id: 'P005', nom: 'Ecran 27"',stock: 2,  prixUnitaire: 2200 },
+      { id: 'P004', nom: 'Ecran 24"', stock: 8, prixUnitaire: 1500 },
+      { id: 'P005', nom: 'Ecran 27"', stock: 2, prixUnitaire: 2200 },
     ]
   },
   'Zone-B': {
     'Rayon-B1': [
-      { id: 'P006', nom: 'Cable HDMI',stock: 100,prixUnitaire: 30  },
-      { id: 'P007', nom: 'Hub USB',   stock: 25, prixUnitaire: 95  },
+      { id: 'P006', nom: 'Cable HDMI', stock: 100, prixUnitaire: 30 },
+      { id: 'P007', nom: 'Hub USB', stock: 25, prixUnitaire: 95 },
     ],
     'Rayon-B2': [
-      { id: 'P008', nom: 'Casque BT', stock: 4,  prixUnitaire: 350 },
-      { id: 'P009', nom: 'Enceinte',  stock: 0,  prixUnitaire: 280 },
+      { id: 'P008', nom: 'Casque BT', stock: 4, prixUnitaire: 350 },
+      { id: 'P009', nom: 'Enceinte', stock: 0, prixUnitaire: 280 },
     ]
   },
   'Zone-C': {
     'Rayon-C1': [
-      { id: 'P010', nom: 'Tapis souris',stock: 60, prixUnitaire: 40 },
-      { id: 'P011', nom: 'Repose-poignet',stock:15,prixUnitaire: 55 },
+      { id: 'P010', nom: 'Tapis souris', stock: 60, prixUnitaire: 40 },
+      { id: 'P011', nom: 'Repose-poignet', stock: 15, prixUnitaire: 55 },
     ]
   }
 };
 
 function trouverProduit(entrepot, idProduit) {
-  // TODO
+  for (const zone in entrepot) {
+    const rayons = entrepot[zone];
+
+    for (const rayon in rayons) {
+      const produits = rayons[rayon];
+
+      const produit = produits.find(p => p.id === idProduit);
+      if (produit) {
+        return produit; 
+      }
+    }
+  }
+
+  return null;
 }
 
 function produitsStockCritique(entrepot, seuilMinimum) {
